27dc24fd457e8dffdf8cce929347bb8afa881c45
Ubuntu <jhhernan@TopV1.khir1z4s25dupjpsqc1zuq4g5a.bx.internal.cloudapp.net>
2020-07-05 20:57:23 +0000

    Bugs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+    this.handleName=this.handleName.bind(this);
+    this.handleLastName=this.handleLastName.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this);
     this.state = {
       "first-name":"",
       "last-name":"",
@@ -10,14 +13,21 @@ class App extends Component {
     }
   }
 
-  handleInput = (event) => {
-    console.log("Entre en el handleInput", event.target.value);
+  handleName(event){
+
     this.setState({
-      [event.target.name]: event.target.value,
+      'first-name': event.target.value,
     }, ()=>{console.log(this.state["first-name"])})
   }
 
-  handleSubmit = (event) => {
+  handleLastName(event){
+    this.setState({
+      'last-name': event.target.value,
+    }, ()=>{console.log(this.state["last-name"])})
+
+  }
+
+  handleSubmit(event){
     event.preventDefault();
     let oldList = this.state.list;
     this.setState({
@@ -30,15 +40,15 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={(e) => this.handleSubmit(e)}>
               <div className="form-group">
-                <label htmlFor="first-name">Nombre</label>
-                <input type="text" value={this.state["first-name"]} onChange={this.handleInput} className="form-control" name="first-name" />
+                <label htmlFor="first-name">Nombrex</label>
+                <input type="text" value={this.state["first-name"]} onChange={this.handleName} className="form-control" name="first-name" />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" value={this.state["last-name"]} onChange={this.handleInput} className="form-control" name="last-name" />
+                <input type="text" value={this.state["last-name"]} onChange={this.handleLastName} className="form-control" name="last-name" />
               </div>
 
               <div className="action">
--- src/App.test.js
+++ src/App.test.js
@@ -41,7 +41,8 @@ it('creates a new guest when submitting the form', () => {
       reset: () => {}
     }
   })
- // expect(wrapper.find("table tr").length).toBe(2)
- // expect(wrapper.find("td").first().text()).toBe('Juan')
- // expect(wrapper.find("td").last().text()).toBe('Gomez')
+  console.log(wrapper.debug())
+  expect(wrapper.find("table tr").length).toBe(2)
+  expect(wrapper.find("td").first().text()).toBe('Juan')
+  expect(wrapper.find("td").last().text()).toBe('Gomez')
 })
